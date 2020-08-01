36a02958fdbefaf45b52dfb947b2267f8a1046f2
stakhivroman <stakhivromann@gmail.com>
2020-08-01 16:51:31 +0300

    add private route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import store from "@/store";
 
 Vue.use(VueRouter);
 
@@ -18,11 +19,49 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: () =>
+      import(/* webpackChunkName: "login" */ "../views/Login.vue")
+  },
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    meta: {
+      requiresAuth: true
+    },
+    component: () =>
+      import(/* webpackChunkName: "dashboard" */ "../views/Dashboard.vue")
   }
 ];
 
 const router = new VueRouter({
   routes
+});
+
+router.beforeEach((to, from, next) => {
+  const { user } = store.state;
+
+  if (to.meta.requiresAuth) {
+    if (user.isAuth) {
+      next();
+    } else {
+      next("login");
+    }
+  } else if (to.name === "Login") {
+    if (user.isAuth) {
+      next("dashboard");
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+  console.log("to", to);
+  console.log("from", from);
+  console.log("next", next);
 });
 
 export default router;
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="Dashboard">
+    <h1>Dashboard</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Dashboard"
+};
+</script>
+
+<style scoped></style>
